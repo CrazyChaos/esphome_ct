e9776864a5e4c0e88cacf66ec252308b51e138dd
CrazyChaos <crazychaos@hotmail.com>
2025-06-05 11:08:57 +0000

    Update fan.cpp
    Fan logic LastSpeed to Speed

--- components/cc1101/fan.cpp
+++ components/cc1101/fan.cpp
@@ -130,7 +130,7 @@ void CC1101Fan::control(const fan::FanCall &call) {
 
 void CC1101Fan::set_fan_speed(uint8_t speed) {
   ESP_LOGD("cc1101_fan", "RF called with %d while last is %d and speed assumed at %d", speed, this->LastSpeed, this->Speed);
-  if (speed != this->LastSpeed ) {
+  if (speed != this->Speed ) {
     // Handle speed control
     switch (speed) {
       case 4:
